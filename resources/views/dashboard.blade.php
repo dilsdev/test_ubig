2d52a1b8a5bf51d297ee575629bd68ee5b64a4bf
abdil <abdil150507@gmail.com>
2024-07-20 19:45:22 +0700

    Menambahkan halaman login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,19 +7,19 @@
     <div class="row row-cards">
       <div class="col-sm-6 col-lg-4">
         <div class="card card-sm">
-          <div class="card-body">
+          <div class="card-body d-flex" style="height: 100px; align-items: center;">
             <div class="row align-items-center">
-              <div class="col-auto">
-                <span class="bg-primary text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/currency-dollar -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"></path><path d="M12 3v3m0 12v3"></path></svg>
+              <div class="col-auto" >
+                <span class="avatar" style="width: 60px; height: 60px;">
+                  <img src="{{ asset('group.png') }}" width="50" height="50" alt="" style="box-sizing: border-box">
                 </span>
               </div>
               <div class="col">
                 <div class="font-weight-medium">
-                  132 Sales
+                  Total
                 </div>
                 <div class="text-secondary">
-                  12 waiting payments
+                  60 Siswa
                 </div>
               </div>
             </div>
@@ -28,19 +28,19 @@
       </div>
       <div class="col-sm-6 col-lg-4">
         <div class="card card-sm">
-          <div class="card-body">
+          <div class="card-body d-flex" style="height: 100px; align-items: center;">
             <div class="row align-items-center">
-              <div class="col-auto">
-                <span class="bg-green text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path><path d="M17 17h-11v-14h-2"></path><path d="M6 5l14 1l-1 7h-13"></path></svg>
+              <div class="col-auto" >
+                <span class="avatar" style="width: 60px; height: 60px;">
+                  <img src="{{ asset('pria.png') }}" width="50" height="50" alt="" style="box-sizing: border-box">
                 </span>
               </div>
               <div class="col">
                 <div class="font-weight-medium">
-                  78 Orders
+                  Laki-Laki
                 </div>
                 <div class="text-secondary">
-                  32 shipped
+                  50
                 </div>
               </div>
             </div>
@@ -49,19 +49,19 @@
       </div>
       <div class="col-sm-6 col-lg-4">
         <div class="card card-sm">
-          <div class="card-body">
+          <div class="card-body d-flex" style="height: 100px; align-items: center;">
             <div class="row align-items-center">
-              <div class="col-auto">
-                <span class="bg-x text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/brand-x -->
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 4l11.733 16h4.267l-11.733 -16z"></path><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path></svg>
+              <div class="col-auto" >
+                <span class="avatar" style="width: 60px; height: 60px;">
+                  <img src="{{ asset('wanita.png') }}" width="50" height="50" alt="" style="box-sizing: border-box">
                 </span>
               </div>
               <div class="col">
                 <div class="font-weight-medium">
-                  623 Shares
+                  Perempuan
                 </div>
                 <div class="text-secondary">
-                  16 today
+                  10
                 </div>
               </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function () {
+    return view('login');
+});
 Route::get('/', function () {
     return view('dashboard');
 });
